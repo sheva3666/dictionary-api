835fcf010e461ee944a3b8933c78167767dc3788
sheva3666 <ivan.shevchenko@sap.com>
2023-03-20 14:44:05 +0100

    initialized project

---
+++ src/main/kotlin/dto/User.kt
@@ -0,0 +1,12 @@
+package dto
+
+import kotlinx.serialization.Serializable
+
+
+@Serializable
+data class User(
+    val id: String,
+    val name: String,
+    val email: String,
+    val password: String
+)
